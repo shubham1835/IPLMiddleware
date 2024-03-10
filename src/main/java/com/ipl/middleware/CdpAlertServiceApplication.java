bc7a2a61c4e24cf5fba9966d43e5772b05273096
shubhamgosewade <shubham1835@gmail.com>
2024-03-10 15:08:04 +0530

    IPL changes

--- src/main/java/com/ipl/middleware/CdpAlertServiceApplication.java
+++ src/main/java/com/ipl/middleware/CdpAlertServiceApplication.java
@@ -2,10 +2,6 @@ package com.ipl.middleware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 //@EnableEurekaClient
@@ -14,17 +10,6 @@ public class CdpAlertServiceApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CdpAlertServiceApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/").allowedOrigins("http://localhost:3000");
-			}
-		};
 	}
 
 }
--- src/main/java/com/ipl/middleware/exception/GlobleExceptionHandler.java
+++ src/main/java/com/ipl/middleware/exception/GlobleExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ipl.middleware.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -25,7 +26,7 @@ public class GlobleExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         List<String> details = new ArrayList<>();
         for(ObjectError error : ex.getBindingResult().getAllErrors()) {
             details.add(error.getDefaultMessage());
